16b571d83f8e74ccec936d399dee5460893eaeaf
sawasthi4691 <Saurabh924@outlook.com>
2020-05-17 12:53:03 +0530

    Added Jsp Web Application.

---
+++ testWebJsp/src/main/java/com/example/TestWebJspApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebJspApplication.class, args);
+	}
+
+}
---
+++ testWebJsp/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html lang="en">
+<body>
+    <div>
+        <div>
+            <h1>Spring Boot JSP Example</h1>
+            
+        </div>
+    </div>
+</body>
+</html>
